49661676fb29c48f13cfd96a625dd270aedca67b
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2021-03-05 17:06:13 -0500

    Room class complete

--- Models/Room.cs
+++ Models/Room.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -12,6 +13,12 @@ namespace PASSION_PROJECT_MVP.Models
         public decimal RoomWidth { get; set; }
         public bool Demolition { get; set; }
         public int NewWalls { get; set; }
+        public string Drywall { get; set; }
+        public string Floor { get; set; }
+        public byte[] Image { get; set; }
+        [ForeignKey("House")]
+        public int HouseId { get; set; }
+        public House House { get; set; }
 
     }
 }
